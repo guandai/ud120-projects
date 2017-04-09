cff20c54404984a96aa3c1160654378642bc9d13
guandai <guandai@hotmail.com>
2017-04-09 23:54:09 +0200

    text_learning

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -42,20 +42,29 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
         temp_counter += 1
-        if temp_counter < 200:
+        print temp_counter
+        if temp_counter > 0:            
             path = os.path.join('..', path[:-1])
-            print path
             email = open(path, "r")
-
+            
             ### use parseOutText to extract the text from the opened email
+            parsed_email =  parseOutText(email)
+            # .encode('ascii')
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
-
-            ### append the text to word_data
+            names = ["sara", "shackleton", "chris", "germani"]
+            for name in names:
+                parsed_email = parsed_email.replace(name, "")
+            ## append the text to word_data
+            word_data.append(parsed_email)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            # print from_person.name
+            if from_person.name == 'from_sara.txt':
+                from_data.append(0)
+            if from_person.name == 'from_chris.txt':
+                from_data.append(1)
 
             email.close()
 
@@ -63,13 +72,48 @@ print "emails processed"
 from_sara.close()
 from_chris.close()
 
+print word_data[152]
+print word_data.__class__.__name__
+print from_data
+
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
 
-
-
 ### in Part 4, do TfIdf vectorization here
+
+# from sklearn.feature_extraction import stop_words
+# stop_words_eng = stop_words.ENGLISH_STOP_WORDS
+
+# for idx, data in enumerate(word_data):
+#     for sw in stop_words_eng:
+#         data = data.replace(sw, '')
+#     word_data[idx] = data
+
+spliter = '_____'
+file = open("word_data.txt","w") 
+for word in word_data:    
+    file.write(word) 
+    file.write(spliter)
+file.close() 
+file = open("word_data.txt", "r") 
+content = file.read() 
+loaded_word_data = content.split(spliter)
+loaded_word_data.pop()
+
+print word_data == loaded_word_data
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(stop_words="english", lowercase=True)
+
+print '-'*100
+vectorizer.fit_transform(word_data)
+vocab_list = vectorizer.get_feature_names()
+print len(vocab_list)
+print vocab_list[34597]
+### 200 words should have 3805
+### 17578 mails   38785 words
+
 
 
