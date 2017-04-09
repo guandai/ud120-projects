e4c0807ee3097b413b58fd91af0b4ec7022028e8
guandai <guandai@hotmail.com>
2017-04-09 23:54:57 +0200

    svm

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,6 +25,33 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn.svm import SVC
+clf = SVC(kernel="rbf", C=10000)
+
+
+#### now your job is to fit the classifier
+#### using the training features/labels, and to
+#### make a set of predictions on the test data
+features_train = features_train[:len(features_train)] 
+labels_train = labels_train[:len(labels_train)] 
+
+clf.fit(features_train, labels_train)
+
+#### store your predictions in a list named pred
+
+# pred1 = clf.predict(features_test[10])
+# pred2 = clf.predict(features_test[26])
+# pred3 = clf.predict(features_test[50])
+
+# print(pred1)  # 1
+# print(pred2)  # 0
+# print(pred3)  # 1
+
+from sklearn.metrics import accuracy_score
+pred = clf.predict(features_test)
+acc = accuracy_score(pred, labels_test)
+print(len(filter(lambda x: x==1, pred))) #// 877
+
 #########################################################
 
 
