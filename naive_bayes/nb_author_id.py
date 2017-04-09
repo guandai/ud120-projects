0a9919de5e82b86be80e0fb92a8054c84341b052
guandai <guandai@hotmail.com>
2017-04-09 23:53:25 +0200

    naive_bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,6 +26,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+
+clf = GaussianNB()
+
+clf.fit(features_train, labels_train)
+print(clf.predict(features_test))
 
 
 #########################################################
